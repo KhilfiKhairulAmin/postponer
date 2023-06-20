537f79af61b92c51747d7f4a7ea7e94e14a4892e
iNFiENiTE-World <infienite@gmail.com>
2023-06-20 22:57:29 +0800

    Add loop to repetitively perform new prompt

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ import os
 import openai
 
 openai.api_key_path = '.pyenv'
+model = 'text-davinci-003'
 
-response = openai.Completion.create(
-  model="text-davinci-003",
-  prompt="Hi, how are you?",
-  temperature=1,
-  max_tokens=256,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0
-)
-
-print(response.choices[0].text)
+while True:
+  prompt = str(input('User: '))
+  response = openai.Completion.create(
+    model=model,
+    prompt=prompt,
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  print(f'{model}: {response.choices[0].text[1:]} \n')
